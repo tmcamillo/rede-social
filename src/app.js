0a0741abcd94335a7027f99ecef84fa53b012784
cakeno <camilla.noba@gmail.com>
2019-03-25 15:17:56 -0300

    button facebook

---
+++ src/app.js
@@ -0,0 +1,11 @@
+
+FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+});
+
+
+function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
+  }
