89d158192c7c8b10282c98d2f11992814e274bcd
Camilla <camilla.noba@gmail.com>
2019-03-26 17:48:54 -0300

    login w/ e-mail and password ok

---
+++ src/auth.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+    // let authFacebookButton = $("#authFacebookButton")
+
+    $("#exampleEnterBtn").click(function(event) {
+        event.preventDefault();
+
+        let email = $("#exampleInputEmail1").val();
+        let password = $("#exampleInputPassword1").val();
+
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then(function() {
+                window.location = "home.html"
+            })
+            .catch(function (error) {
+
+                var errorCode = error.code;
+                var errorMessage = error.message;
+
+                console.log(errorCode)
+                console.log(errorMessage)
+            })
+    });
+
+    $("#btnRegister").click(function(event) {
+        event.preventDefault();
+        
+        let email = $("#emailInput").val();
+        let password = $("#passwordInput").val();
+
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then(function() {
+                window.location = "home.html"
+            })
+            .catch(function (error){
+
+                var errorCode = error.code;
+                var errorMessage = error.message;
+
+                console.log(errorCode)
+                console.log(errorMessage)
+            })
+    });
+
+})
---
+++ src/config.js
@@ -0,0 +1,29 @@
+var config = {
+    apiKey: "AIzaSyCY0vRXRq3ZgjcWmP6oc73VtGrP97RsALA",
+    authDomain: "redesocial-4b829.firebaseapp.com",
+    databaseURL: "https://redesocial-4b829.firebaseio.com",
+    projectId: "redesocial-4b829",
+    storageBucket: "redesocial-4b829.appspot.com",
+    messagingSenderId: "781389421197"
+  };
+  firebase.initializeApp(config);
+  
+//   window.fbAsyncInit = function() {
+//       FB.init({
+//         appId      : '165818190981328',
+//         cookie     : true,
+//         xfbml      : true,
+//         version    : 'v3.2'
+//       });
+        
+//       FB.AppEvents.logPageView();   
+        
+//     };
+  
+//     (function(d, s, id){
+//        var js, fjs = d.getElementsByTagName(s)[0];
+//        if (d.getElementById(id)) {return;}
+//        js = d.createElement(s); js.id = id;
+//        js.src = "https://connect.facebook.net/en_US/sdk.js";
+//        fjs.parentNode.insertBefore(js, fjs);
+//      }(document, 'script', 'facebook-jssdk'));
