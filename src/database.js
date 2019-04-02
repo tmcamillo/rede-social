faf1eda20e65f87540685da3d8e5337dbbd226c8
Camilla <camilla.noba@gmail.com>
2019-04-02 17:11:14 -0300

    index layout ok

--- src/database.js
+++ src/database.js
@@ -1,28 +1,52 @@
+const database = firebase.database();
+// const USER_ID = window.location.search.match(/\?id=(.*)/)[1];
 const photoFile = document.getElementById('photoFile');
 const nameInput = document.getElementById('nameInput');
 const lastNameInput = document.getElementById("lastNameInput");
 const emailInput = document.getElementById("emailInput");
 const phoneInput = document.getElementById('phoneInput');
 const passwordInput = document.getElementById('passwordInput');
-const confirmPasswordInput = document.getElementById('confirmPasswordInput');
-const btnRegister = document.getElementById('btnRegister');
 
-// Ao clicar no botão
-btnRegister.addEventListener('click', function () {
-    create(nameInput.value, lastNameInput.value, emailInpunt.value, phoneInput.value, passwordInput.value, confirmPasswordInput.value);
-});
-
-// Função para criar um registro no Firebase
-function create(photo, name, lastName, email, phone, password, confirmPassword) {
-    let data = {
-        photo: photo,
-        name: name,
-        lastName: lastName,
+function writeUserData(userId, imageUrl, name, lastName, email, phone, password) {
+    database.ref('users/' + userId).set({
+        profile_picture: imageUrl,
+        username: name,
+        userLastName: lastName,
         email: email,
         phone: phone,
-        password: password,
-        confirmPassword: confirmPassword
-    };
-
-    return firebase.database().ref().child('users').push(data);
+        pass: password
+    });
 }
+
+$(document).ready(function(){
+
+    database.ref("/post/").once("value")
+    .then(function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+            let childkey = childSnapshot.key;
+            let childData = childSnapshot.val();
+            $(".post-list").append(`<li>${childData.label}<br>${childData.review}</li>`);
+            
+        })
+    });
+
+    
+
+    $(".add-post").click(function(event) {
+        event.preventDefault();
+        let newBrand = $("#label").val();
+        let brandUpperCase = newBrand.toUpperCase();
+        let newPost =  $("#comment").val();
+        let alcohoolPer = $("#alcohol").val();
+
+        
+        database.ref("post/").push({
+            label: brandUpperCase,
+            review: newPost,
+            alcohoolPer: alcohoolPer
+        });
+        
+        $(".post-list").append(`<li>${brandUpperCase}<br>${newPost}<br>${alcohoolPer}%</li>`);
+    });
+
+});
