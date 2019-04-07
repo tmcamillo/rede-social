0321b7a66f9c4427896395aaef205fda1879e556
Camilla <camilla.noba@gmail.com>
2019-04-07 18:46:55 -0300

    filter private post ok

--- src/database.js
+++ src/database.js
@@ -1,6 +1,17 @@
 const database = firebase.database();
 const USER_ID = window.location.search.match(/\?id=(.*)/)[1];
+function loadPosts(){
+	database.ref("/post/" + USER_ID).once("value")
+	.then(function (snapshot) {
+		snapshot.forEach(function (childSnapshot) {
+			let childkey = childSnapshot.key;
+			let childData = childSnapshot.val();
 
+			appendData(childData, childkey)
+		})
+
+	});
+}
 function writeUserData(email, password, uid) {
 
 	database.ref("users/" + uid).set({
@@ -16,10 +27,10 @@ function time() {
 	let today = new Date();
 	let hour = today.getHours();
 	let min = today.getMinutes();
-	let day = today.getDay();
-	let month = today.getMonth();
-	let year = today.getFullYear();
-	let timeNow = day + "/" + month + "/" + year + " - " + leftZeros(hour) + ":" + leftZeros(min) ;
+	let day = today.getUTCDate();
+	let month = today.getUTCMonth();
+	let year = today.getUTCFullYear();
+	let timeNow = leftZeros(day) + "/" + leftZeros(month) + "/" + year + " - " + leftZeros(hour) + ":" + leftZeros(min);
 
 	return timeNow;
 }
@@ -37,7 +48,7 @@ function appendData(childData, childKey) {
 	$(".post-list").append(
 		`
 		<li>
-		<div class="container-fluid col-md-6 bg-light rounded p-4 my-3">
+		<div class="container-fluid col-md-6 bg-light rounded p-4 mb-3">
 			<div class="d-flex">
 				<!-- <img src=""> -->
 				<a class="d-inline-flex mr-auto mb-3">
@@ -70,51 +81,69 @@ function appendData(childData, childKey) {
 }
 
 $(document).ready(function () {
-
+	loadPosts()
 	ratingStar()
 	// gettingDrinks()
 
-	database.ref("/post/" + USER_ID).once("value")
-		.then(function (snapshot) {
-			snapshot.forEach(function (childSnapshot) {
-				let childkey = childSnapshot.key;
-				let childData = childSnapshot.val();
+	// database.ref("/post/" + USER_ID).once("value")
+	// .then(function (snapshot) {
+	// 	snapshot.forEach(function (childSnapshot) {
+	// 		let childkey = childSnapshot.key;
+	// 		let childData = childSnapshot.val();
 
-				appendData(childData, childkey)
-			});
-		});
+	// 		appendData(childData, childkey)
+	// 	})
 
-	$(".add-post").click(function (event) {
-		event.preventDefault();
-		// let drink = parseInt($('#listDrinks li.selected').last().data('value'), 10);
-		// let iDrink = iconDrink(drink);
-		// let ratingValue = parseInt($('#stars li.selected').last().data('value'), 10);
+	// });
 
-		let data = {
-			// drinkIcon: iconDrink(parseInt($('#listDrinks li.selected').last().data('value'), 10)),
-			label: $("#label").val().toUpperCase(),
-			review: $("#comment").val(),
-			alcohoolPer: $("#alcohol").val(),
-			postTime: time(),
-			starScore: parseInt($('#stars li.selected').last().data('value'), 10),
-			privacy: $("#selPrivacy option:selected").val(),
-		};
-
-		console.log(data)
-		let postFromDb = database.ref("/post/" + USER_ID).push(data);
-		appendData(data, postFromDb.key)
-		$('#container-comment')[0].reset();
-	});
-
-	let selected_key = ''
-
-	$(document).on('click', '.trash-ic', function () {
-		selected_key = $(this).attr('data-post-id');
-	})
-
-	$("#btnDelete").click(function () {
-		database.ref("post/" + USER_ID + "/" + selected_key).remove();
-		$(`a[data-post-id=${selected_key}]`).closest("li").remove();
-		$('#deleteModal').modal('hide');
-	});
 });
+$("#news-feed").click(function () {
+	$('#post-list').html("");
+	loadPosts()
+})
+
+$("#private-post").click(function () {
+	$('#post-list').html("");
+	database.ref("/post/" + USER_ID).orderByChild("privacy").equalTo('privado').once('value', function (snapshot) {
+		snapshot.forEach(function (childSnapshot) {
+			let childkey = childSnapshot.key;
+			let childData = childSnapshot.val();
+			appendData(childData, childkey)
+		});
+	});
+})
+
+$(".add-post").click(function (event) {
+	event.preventDefault();
+	// let drink = parseInt($('#listDrinks li.selected').last().data('value'), 10);
+	// let iDrink = iconDrink(drink);
+	// let ratingValue = parseInt($('#stars li.selected').last().data('value'), 10);
+
+	let data = {
+		// drinkIcon: iconDrink(parseInt($('#listDrinks li.selected').last().data('value'), 10)),
+		label: $("#label").val().toUpperCase(),
+		review: $("#comment").val(),
+		alcohoolPer: $("#alcohol").val(),
+		postTime: time(),
+		starScore: parseInt($('#stars li.selected').last().data('value'), 10),
+		privacy: $("#selPrivacy option:selected").val(),
+	};
+
+	let postFromDb = database.ref("/post/" + USER_ID).push(data);
+
+	appendData(data, postFromDb.key)
+	$('#container-comment')[0].reset();
+});
+
+let selected_key = ''
+
+$(document).on('click', '.trash-ic', function () {
+	selected_key = $(this).attr('data-post-id');
+})
+
+$("#btnDelete").click(function () {
+	database.ref("post/" + USER_ID + "/" + selected_key).remove();
+	$(`a[data-post-id=${selected_key}]`).closest("li").remove();
+	$('#deleteModal').modal('hide');
+});
+
