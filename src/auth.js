b3ee6943bebd93bfcf58b35001947108ca64f10b
tmcamillo <imeio.tmc@gmail.com>
2019-04-09 14:26:17 -0300

    aspas simples, form de editar in-place formatado e função logout incluidos

--- src/auth.js
+++ src/auth.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip()
 
-    $("#sign-in-btn").click(function (event) {
+    $('#sign-in-btn').click(function (event) {
         event.preventDefault();
 
-        let email = $("#email-input").val();
-        let password = $("#password-input").val();
+        let email = $('#email-input').val();
+        let password = $('#password-input').val();
 
         firebase.auth().signInWithEmailAndPassword(email, password)
             .then(function (response) {
-                window.location = "home.html?id=" + response.user.uid;
+                window.location = 'home.html?id=' + response.user.uid;
             })
 
             .catch(function (error) {
@@ -20,25 +20,33 @@ $(document).ready(function () {
     });
 
 
-    $("#sign-up-btn").click(function (event) {
+    $('#sign-up-btn').click(function (event) {
         event.preventDefault();
 
-        let email = $("#emailInput").val();
-        let password = $("#passwordInput").val();
+        let email = $('#emailInput').val();
+        let password = $('#passwordInput').val();
 
         firebase.auth().createUserWithEmailAndPassword(email, password)
             .then(function (response) {
                 writeUserData(email, password, response.user.uid);
-                window.location = "home.html?id=" + response.user.uid;
+                window.location = 'home.html?id=' + response.user.uid;
             })
 
             .catch(function (error) {
-
                 let errorCode = error.code;
                 let errorMessage = error.message;
-
-                console.log(errorCode)
-                console.log(errorMessage)
+                alert(errorCode, errorMessage);
+               
             })
     });
+    
+
+    $('#logout-btn').click(function() {
+        firebase.auth().signOut().then(function() {
+            window.location = 'index.html'
+          }).catch(function(error) {
+           alert('Ocorreu um erro, tente novamente.')
+          });
+    })
+        
 });
