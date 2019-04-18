7cece4ca63d4e8e1a424eb571cf81f68c7156e0f
Camilla <camilla.noba@gmail.com>
2019-04-18 15:09:55 -0300

    readme

--- public/counter.js
+++ public/counter.js
@@ -1,8 +1,7 @@
 function badges(){
   let postCounter = $('#post-list').children().length;
-if(postCounter < 2){
+if(postCounter < 1){
     $("#no-post").html("");
-    $(".badge-user").html("");
 } else{
   $(".badge-user").html(`<img src="./assets/badgeAtivo 1.png" class="badge-user">`);
 }
